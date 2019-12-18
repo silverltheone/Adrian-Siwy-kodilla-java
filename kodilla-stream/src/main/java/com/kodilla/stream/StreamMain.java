39337e07cb796b1aff5070cd0d3010d947993e4f
silverltheone <adeksiwy@gmail.com>
2019-12-18 18:24:54 +0100

    Task 7,1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+
+import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        expressionExecutor.executeExpression(10,5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10,5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10,5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10,5, (a, b) -> a / b);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Kiedy przyjdą podpalić dom", (poem -> "ABC "  + poem + " CBA"));
+        poemBeautifier.beautify("Litwo ojczyzna moja", (poem -> toUpperCase(poem)));
+        poemBeautifier.beautify("Jak kamienie przez Boga rzucane na szaniec", (poem -> "***" + poem + "***"));
+        poemBeautifier.beautify("WLAZ KOTEK NA PLOTEK", (poem -> toLowerCase(poem)));
+    }
+}
