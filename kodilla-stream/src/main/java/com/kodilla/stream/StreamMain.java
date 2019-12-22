1515012d040b7b31a98845231db593b78e9e60c7
silverltheone <adeksiwy@gmail.com>
2019-12-22 17:55:38 +0100

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 
 import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
@@ -19,5 +20,10 @@ public class StreamMain {
         poemBeautifier.beautify("Litwo ojczyzna moja", (poem -> toUpperCase(poem)));
         poemBeautifier.beautify("Jak kamienie przez Boga rzucane na szaniec", (poem -> "***" + poem + "***"));
         poemBeautifier.beautify("WLAZ KOTEK NA PLOTEK", (poem -> toLowerCase(poem)));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
+
+
 }
