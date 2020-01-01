13dba22e4bdaff8806919f7f65ca7515472b89d5
silverltheone <adeksiwy@gmail.com>
2020-01-01 19:58:39 +0100

    8.2

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,7 +1,27 @@
 package com.kodilla.exception.io;
 
-public class FileReader {
-    public void readFile(){
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
+public class FileReader {
+    public void readFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+
+        try(Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))){
+
+            fileLines.forEach(System.out::println);
+
+        } catch (IOException e) {
+
+            System.out.println("PROBLEM!!!" + e);
+
+        } finally {
+            System.out.println("I'm gonna be here . . . always");
+        }
     }
 }
