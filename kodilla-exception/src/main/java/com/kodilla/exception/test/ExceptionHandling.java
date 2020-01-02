774cc407f9f9b115e55d33c474210cb5bfd1ecf6
silverltheone <adeksiwy@gmail.com>
2020-01-02 18:49:19 +0100

    8.3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,13 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(2.5, 1.5);
+        } catch (Exception e) {
+            System.out.println("Problem!!");
+        }
+    }
+}
